3588444d0eda4cb58c186f752d025efdcaa9c162
Victor Adeshile <davik4life@gmail.com>
2021-02-13 19:24:13 +0100

    Worked on Gatsby 100day challenge

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+const about = () => (
+	<main>
+		<h1> Welcome to the About Us Page</h1>
+		<p> The best Audio headphone Company in Nigeria. <br /> You can rent our headphone for a small fee. </p>
+	</main>
+);
+
+export default about;
---
+++ src/pages/location/{contentfulCity.name}.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { graphql } from "gatsby";
+
+
+export const query = graphql `
+    query ($id: String = "") {
+        contentfulCity (id: {eq: $id}){
+            name
+            description
+            location {
+                lat
+                lon
+        }
+      }
+    }
+  
+
+`;
+
+
+export default function Location ({data}){
+	return (<div>
+		<h1>
+        Hi, {data.contentfulCity.name}
+		</h1>
+		<h2>Alert!!!, {data.contentfulCity.description}</h2>
+		<h3>Our Address Cordinate is at , {data.contentfulCity.location.lat} - {data.contentfulCity.location.lon}</h3>
+	</div>);
+}
+
+
