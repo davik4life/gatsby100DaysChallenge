1383f6253ee6670d69370f1c2cc74018f0c7c1a3
Victor Adeshile <davik4life@gmail.com>
2021-02-13 21:17:24 +0100

    Created a branch and then added back to Home functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,47 @@
 import * as React from "react";
+import { Link, graphql } from "gatsby";
 
-const home = () => (
-	<main>
-		<h1> Welcome to the Homepage</h1>
-		<p> I have added some paragraphs here. </p>
-	</main>
-);
+export const query = graphql `
+query MyQuery {
+  allContentfulCity {
+    edges {
+      node {
+        name
+        description
+        location {
+          lat
+          lon
+        }
+        gatsbyPath(filePath: "/location/{contentfulCity.name}")
+      }
+    }
+  }
+}
 
-export default home;
+`;
+
+
+export default function Home({data}){
+	return (
+		<div>
+			<h1> Welcome to the Homepage</h1>
+			<p> We are open for business </p>
+			<h1>Checkout our current location below;</h1>
+    
+			<ul>
+				{
+					data.allContentfulCity.edges.map(({node:city}) => (
+						<li key={city.name}>
+							<Link to= {city.gatsbyPath}>{city.name}</Link> - {city.description} - {city.location.lat} - {city.location.lon}
+						</li>
+					))
+				}
+
+			</ul>
+      
+		</div>
+	);
+}
+
+
+
--- src/pages/location/{contentfulCity.name}.js
+++ src/pages/location/{contentfulCity.name}.js
@@ -20,12 +20,12 @@ export const query = graphql `
 
 export default function Location ({data}){
 	return (<div>
-		<h1>
-        Hi, {data.contentfulCity.name}
-		</h1>
+		<h1> Hi, {data.contentfulCity.name} </h1>
 		<h2>Alert!!!, {data.contentfulCity.description}</h2>
 		<h3>Our Address Cordinate is at , {data.contentfulCity.location.lat} - {data.contentfulCity.location.lon}</h3>
-	</div>);
+		<button><a href="/">Back to HomePage</a></button>
+	</div>
+	);
 }
 
 
